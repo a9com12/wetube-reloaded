2ee54fee3513121909e732a95c18f356479d4f4f
a9com12 <a9com12@gmail.com>
2022-12-29 17:19:39 +0800

    login.updated

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
 email: {type:String, required: true, unique:true},
@@ -6,6 +7,13 @@ username: {type:String, required: true, unique: true},
 password: {type:String, required:true},
 name: {type:String, required:true},
 location: String,
+});
+
+
+userSchema.pre('save', async function() {
+    console.log(this.password);
+    this.password = await bcrypt.hash(this.password, 5);
+    console.log(this.password);
 });
 
 const User = mongoose.model('User', userSchema);
