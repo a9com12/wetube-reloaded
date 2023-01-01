33a7b6990d479849899238ff1c93de12acfc4976
a9com12 <a9com12@gmail.com>
2023-01-01 23:35:12 +0800

    Update videoController.js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -20,7 +20,7 @@ export const getEdit = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
     if(!video ) {
-        return res.status(404).render("404", {pageTitle:"Video not found."});
+        return res.status(404).render("404", {pageTitle:"Video not found yet."});
     }
     return res.render("edit", { pageTitle: `Edit: ${video.title}`, video});
   };
