45b562ea6ce37c7d484d7719341b86f4db2d75cb
a9com12 <a9com12@gmail.com>
2023-01-16 23:49:07 +0800

    video.upload

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -48,12 +48,14 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req,res) => {
+    const {path: fileUrl}= req.file;
     const { title, description, hashtags } = req.body;
 
     try {
         await Video.create({
             title,
             description,
+            fileUrl,
             hashtags: Video.formatHashtags(hashtags),
 
           });
