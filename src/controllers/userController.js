52a69d41eef6756fc774781f1beeb662c3c4c438
a9com12 <a9com12@gmail.com>
2023-01-17 22:28:22 +0800

    userprofile.update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -188,5 +188,13 @@ await user.save();
 
 
 
-export const see =(req, res) => res.send("See User");
+export const see = async (req, res) => {
+  const {id} = req.params;
+  const user = await User.findById(id);
+  if (!user) {
+    return res.status(404).render("404", { pageTitle: "User not found." });
+  }
+  return res.render("users/profile", {pageTitle: user.name, user})
+
+}
 
