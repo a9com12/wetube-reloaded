c48e03575887dd844adc5abbe03ab0946bdbc272
a9com12 <a9com12@gmail.com>
2023-01-17 23:03:57 +0800

    owner.updated

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Wetube Reloaded
+
+1. watch.pug 에서 if video.ower 와 loggedInUser.\_id 가 일치하면 Edit video 와 delete video 를보여주게한다.
+2. videoSchema에 owner를 추가한다. reference를 User로 추가하고 import 한다. video와 user가 연결된다.
+3. postUpload 에 owner:\_id 를 추가한다.
+4. watch 안에 owner 변수를 추가한다. User.findById 를 넣어준다. 여기선 이미 서로 연결됨
+5. watch.pug 에서 uploaded by owner.name 을 해준다.
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,5 @@
 import Video from "../models/Video";
+import User from "../models/User";
 
 
 export const home = async(req, res) => { 
@@ -9,10 +10,11 @@ export const home = async(req, res) => {
 export const watch = async (req, res) => {
     const {id} =req.params;
     const video = await Video.findById(id);
+    const owner = await User.findById(video.owner);
     if(!video ) {
         return res.render("404", {pageTitle:"Video not found."});
     }
-    return res.render("watch", {pageTitle: video.title ,video});
+    return res.render("watch", {pageTitle: video.title ,video, owner});
 };
 
 
@@ -48,6 +50,9 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req,res) => {
+  const {
+    user: {_id},
+  }= req.session;
     const {path: fileUrl}= req.file;
     const { title, description, hashtags } = req.body;
 
@@ -56,6 +61,7 @@ export const postUpload = async (req,res) => {
             title,
             description,
             fileUrl,
+            owner:_id,
             hashtags: Video.formatHashtags(hashtags),
 
           });
