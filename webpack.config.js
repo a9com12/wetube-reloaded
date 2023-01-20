16a6bbce3d5ea041164c2e7120cf4af2a3362e8a
a9com12 <a9com12@gmail.com>
2023-01-20 16:32:55 +0800

    scss.update

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-    entry: "./src/client/js/main.js",
+    entry: {
+      main: "./src/client/js/main.js",
+      videoPlayer: "./src/client/js/videoPlayer.js"
+    },
+    watch: true,
+    plugins: [new MiniCssExtractPlugin({
+        filename: "css/styles.css",
+    })],
     mode: "development",
     output: {
-      filename: "main.js",
-      path: path.resolve(__dirname, "assets", "js"),
+      filename: "js/[name].js",
+      path: path.resolve(__dirname, "assets"),
+      clean:true,
     },
     module: {
       rules: [
@@ -18,6 +27,10 @@ module.exports = {
             },
           },
         },
+        {
+            test: /\.scss$/,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        }
       ],
     },
   };
