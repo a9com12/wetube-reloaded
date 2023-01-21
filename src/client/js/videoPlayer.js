7af37c04cb44c294b76dcc749ea95c841793adbc
a9com12 <a9com12@gmail.com>
2023-01-21 21:19:44 +0800

    video.update

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,58 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volumeRange = document.getElementById("volume");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
+};
+
+const handleMuteClick = (e) => {
+  if (video.muted) {
+    video.muted = false;
+    video.volume = 0.5;
+  } else {
+    video.muted = true;
+    video.volume = 0;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+  if (Number(value) === 0) {
+    muteBtn.innerText = "Unmute";
+    video.muted = true;
+    } else {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+    }
+};
+
+
+
+
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
