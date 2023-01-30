c1cd969a27f0520e01206847972e2860b9c23035
a9com12 <a9com12@gmail.com>
2023-01-30 20:04:51 +0800

    db.update

--- src/db.js
+++ src/db.js
@@ -2,8 +2,11 @@
 import mongoose from 'mongoose';
 
 mongoose.connect(process.env.DB_URL, {
-   useNewUrlParser: true, 
-   useUnifiedTopology: true, 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+}, err => {
+  if(err) throw err;
+  console.log('Connected to MongoDB!!!')
 });
 
 
