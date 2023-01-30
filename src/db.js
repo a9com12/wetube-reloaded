85ea5a24641ecc8ff7bc6b0865d5ea8bab0761bd
a9com12 <a9com12@gmail.com>
2023-01-30 20:01:59 +0800

    db.update

--- src/db.js
+++ src/db.js
@@ -2,10 +2,8 @@
 import mongoose from 'mongoose';
 
 mongoose.connect(process.env.DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: true,
-  useCreateIndex: true,
+   useNewUrlParser: true, 
+   useUnifiedTopology: true, 
 });
 
 
