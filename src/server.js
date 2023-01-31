568c0f18ad35684616bdd21c0769386740a3039c
a9com12 <a9com12@gmail.com>
2023-01-31 11:46:06 +0800

    server.update

--- src/server.js
+++ src/server.js
@@ -26,9 +26,30 @@ app.use(
       store: MongoStore.create({mongoUrl: process.env.DB_URL}),
     })
   );
+
+  app.use((req, res, next) => {
+    req.sessionStore.all((error, sessions) => {
+      // console.log(sessions);
+      next();
+    });
+  });
+  
+  app.use(localMiddleware);
+  app.use(flash());
+  app.use("/upload", express.static("upload"));
+  app.use(
+    "/static",
+    express.static("assets"),
+    express.static("node_modules/@ffmpeg/core/dist")
+  );
+
+  
 app.use((req, res, next) => {
     res.header("Cross-Origin-Embedder-Policy", "require-corp");
     res.header("Cross-Origin-Opener-Policy", "same-origin");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+
     next();
     }); 
   
